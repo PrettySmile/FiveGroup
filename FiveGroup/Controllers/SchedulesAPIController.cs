1da20b6f287efc1cdcc6b51f325605253f46cae7
昊天 袁 <talesa82039@gmail.com>
2019-11-27 10:32:19 +0800

    時間表api 小修

--- FiveGroup/Controllers/SchedulesAPIController.cs
+++ FiveGroup/Controllers/SchedulesAPIController.cs
@@ -34,53 +34,80 @@ namespace FiveGroup.Controllers
         }
         public DataTable GET()
         {
-            var sql = @"SELECT A.*,
-                   B.doc_name,C.dep_name,D.hos_name
+            var sql = @"SELECT A.starttime,A.endtime,
+                   B.doc_name,C.dep_name,B.doc_history,D.hos_name,D.hos_eng_name,D.hos_phone,E.city_name,F.district_name,D.hos_address
             FROM schedule AS A
             LEFT JOIN doctor AS B ON B.doc_id=A.doc_id
             LEFT JOIN department AS C ON C.dep_id=A.dep_id
-            LEFT JOIN hospital AS D ON D.hos_id=A.hos_id";
+            LEFT JOIN hospital AS D ON D.hos_id=A.hos_id
+            LEFT JOIN city AS E ON D.c_id = E.c_id
+            LEFT JOIN district AS F ON D.d_id = F.d_id";
+            sql += " WHERE ";
+            sql += " A.s_display = 'True' and B.doc_display = 'True' and D.hos_display = 'True' ";
             DataTable sc = QuerySql(sql);
 
             return sc;
         }
 
         
-        public DataTable Get(string doc_name, string dep_name, string hos_name, string date)
+        public DataTable Get(string doc_name, string dep_name, string hos_name,string hos_eng_name, string date,string city_name,string district_name)
         {
-            var sql = @"SELECT A.*,
-                   B.doc_name,C.dep_name,D.hos_name
+            var sql = @"SELECT A.starttime,A.endtime,
+                   B.doc_name,C.dep_name,B.doc_history,D.hos_name,D.hos_eng_name,D.hos_phone,E.city_name,F.district_name,D.hos_address
             FROM schedule AS A
             LEFT JOIN doctor AS B ON B.doc_id=A.doc_id
             LEFT JOIN department AS C ON C.dep_id=A.dep_id
-            LEFT JOIN hospital AS D ON D.hos_id=A.hos_id";
-            if (doc_name != null || dep_name != null || hos_name != null || date != null)
+            LEFT JOIN hospital AS D ON D.hos_id=A.hos_id
+            LEFT JOIN city AS E ON D.c_id = E.c_id
+            LEFT JOIN district AS F ON D.d_id = F.d_id";
+            sql += " WHERE ";
+            sql += " A.s_display = 'True' and B.doc_display = 'True' and D.hos_display = 'True' ";
+            if (doc_name != null || dep_name != null || hos_name != null || hos_eng_name != null || date != null || city_name != null || district_name != null)
             {
-                sql += " WHERE ";
+                sql += " and ";
                 if (doc_name != null)
                 {
                     sql += " B.doc_name like '%" + doc_name + "%'";
-                    if (dep_name != null || hos_name != null || date != null)
+                    if (dep_name != null || hos_name != null || hos_eng_name != null || date != null || city_name != null || district_name != null)
                         sql += " and ";
                 }
                 if (dep_name != null)
                 {
                     sql += " C.dep_name like '%" + dep_name + "%'";
-                    if (hos_name != null || date != null)
+                    if (hos_name != null || hos_eng_name != null || date != null || city_name != null || district_name != null)
                         sql += " and ";
                 }
                 if (hos_name != null)
                 {
                     sql += " D.hos_name like '%" + hos_name + "%'";
-                    if (date != null)
+                    if (hos_eng_name != null || date != null || city_name != null || district_name != null)
+                        sql += " and ";
+                }
+                if (hos_eng_name != null)
+                {
+                    sql += " D.hos_eng_name like '%" + hos_eng_name + "%'";
+                    if (date != null || city_name != null || district_name != null)
                         sql += " and ";
                 }
                 if (date != null)
                 {
                     sql += " A.starttime <= CONVERT(datetime,'" + date + "')" +
                          " and A.endtime >= CONVERT(datetime,'" + date + "')";
+                    if (city_name != null || district_name != null)
+                        sql += " and ";
+                }
+                if (city_name != null)
+                {
+                    sql += " E.city_name like '%" + city_name + "%'";
+                    if (district_name != null)
+                        sql += " and ";
+                }
+                if (district_name != null)
+                {
+                    sql += " F.district_name like '%" + district_name + "%'";
                 }
             }
+
 
             DataTable sc = QuerySql(sql);
             if (sc == null)
