57a191940aac88a11f88d4b16522371372e7a321
Jason Wu <bluereindeer@hotmail.com>
2019-12-26 08:48:02 +0800

    添加物查詢下拉式選單

--- FiveGroup/Controllers/IngrediantsController.cs
+++ FiveGroup/Controllers/IngrediantsController.cs
@@ -20,6 +20,28 @@ namespace FiveGroup.Controllers
         // GET: ingrediants
         public ActionResult Index(string search, int? page)
         {
+            List<SelectListItem> items = new List<SelectListItem>();
+
+            items.Add(new SelectListItem { Text = "防腐劑", Value = "(一) 防腐劑", Selected = true });
+            items.Add(new SelectListItem { Text = "殺菌劑", Value = "(二) 殺菌劑" });
+            items.Add(new SelectListItem { Text = "抗氧化劑", Value = "(三) 抗氧化劑"});
+            items.Add(new SelectListItem { Text = "漂白劑", Value = "(四) 漂白劑" });
+            items.Add(new SelectListItem { Text = "保色劑", Value = "(五) 保色劑" });
+            items.Add(new SelectListItem { Text = "膨脹劑", Value = "(六) 膨脹劑" });
+            items.Add(new SelectListItem { Text = "品質改良用、釀造用及食品製造用劑", Value = "(七) 品質改良用、釀造用及食品製造用劑" });
+            items.Add(new SelectListItem { Text = "營養添加劑", Value = "(八) 營養添加劑" });
+            items.Add(new SelectListItem { Text = "著色劑", Value = "(九) 著色劑" });
+            items.Add(new SelectListItem { Text = "香料", Value = "(十) 香料" });
+            items.Add(new SelectListItem { Text = "調味劑", Value = "(十一) 調味劑" });
+            items.Add(new SelectListItem { Text = "粘稠劑（糊料）", Value = "(十二) 粘稠劑（糊料）" });
+            items.Add(new SelectListItem { Text = "結著劑", Value = "(十三) 結著劑" });
+            items.Add(new SelectListItem { Text = "食品工業用化學藥品", Value = "(十四) 食品工業用化學藥品" });
+            items.Add(new SelectListItem { Text = "載體", Value = "(十五) 載體" });
+            items.Add(new SelectListItem { Text = "乳化劑", Value = "(十六) 乳化劑" });
+            items.Add(new SelectListItem { Text = "其他", Value = "(十七) 其他" });
+            items.Add(new SelectListItem { Text = "甜味劑", Value = "(十一之一) 甜味劑" });
+
+            ViewBag.search = items;
 
             List<ingrediant> listmap = db.ingrediant.ToList();
 
